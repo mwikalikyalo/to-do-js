0c183c663f51c6f40f6c92813a579fa88d1949dc
mwikalikyalo <winniemwikali07@gmail.com>
2022-10-24 09:54:47 +0300

    Add the app's structure and styling

---
+++ index.js
@@ -0,0 +1,14 @@
+//get task from the search form
+//display the task on the paragraph
+let btn = document.getElementsByClassName("adding")
+let show = document.getElementsByClassName("showTask")
+
+function display() {
+    let tasks= document.getElementsByClassName("task")
+    
+
+    
+};
+
+//delete button (deletion)
+//edit button (edits)
